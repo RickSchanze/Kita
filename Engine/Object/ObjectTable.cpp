41d37341f0006da6c684862210ceb8ce9180a770
RickSchanze <2815091564@qq.com>
2025-08-21 08:44:21 +0800

    feat(ObjectTable): 实现 ObjectTable 类的基本功能
    - 添加 RegisterObject 方法，用于注册对象
    - 添加 UnregisterObject 方法，用于注销对象
    - 实现 IsObjectAlive 方法，检查对象是否已注册
    - 使用 Map 容器存储对象句柄和对象指针
    -优化头文件包含，提高代码可读性

--- Engine/Object/ObjectTable.cpp
+++ Engine/Object/ObjectTable.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "ObjectTable.h"
+
+bool ObjectTable::RegisterObject(Object* Object) {
+  if (Object == nullptr)
+    return false;
+  Int32 ObjectHandle = Object->GetObjectHandle();
+  if (IsObjectAlive(ObjectHandle)) {
+    LOG_ERROR_TAG("Object", "重复向ObjectTable添加同一ID的Object, Id = {}", ObjectHandle);
+    return false;
+  }
+  mObjectTable.Add(ObjectHandle, Object);
+  return true;
+}
+
+void ObjectTable::UnregisterObject(const Int32 ObjectHandle) {
+  if (IsObjectAlive(ObjectHandle)) {
+    Object* Object = mObjectTable[ObjectHandle];
+    Delete(Object);
+    mObjectTable.Remove(ObjectHandle);
+  }
+}
+
+void ObjectTable::UnregisterObject(const Object* Object) {
+  if (Object != nullptr)
+    UnregisterObject(Object->GetObjectHandle());
+}
--- Engine/Object/ObjectTable.h
+++ Engine/Object/ObjectTable.h
@@ -1,7 +1,21 @@
 #pragma once
+#include "Core/Container/Map.h"
 #include "Core/Singleton/Singleton.h"
+#include "Object.h"
 
-class ObjectTable : public Singleton<ObjectTable>
-{
+class ObjectTable : public Singleton<ObjectTable> {
+public:
+  [[nodiscard]] bool IsObjectAlive(const Int32 Handle) const { return mObjectTable.Contains(Handle); }
 
+  /// 注册一个Object 如果此Object对应的Handle已存在则Log错误并返回false
+  bool RegisterObject(Object* Object);
+
+  /// 从ObjectTable中删除一个Object, 同时释放内存 TODO: 延迟内存释放
+  void UnregisterObject(Int32 ObjectHandle);
+  /// 从ObjectTable中删除一个Object, 同时释放内存
+  void UnregisterObject(const Object* Object);
+
+
+private:
+  Map<Int32, Object*> mObjectTable;
 };
